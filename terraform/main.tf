c6453244315e734ca727b6e4236394b6c6253183
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-07-25 05:46:11 +0900

    added terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,278 @@
+terraform {
+  required_version = ">= 0.12.5"
+  required_providers {
+    sakuracloud = {
+      source  = "sacloud/sakuracloud"
+      version = "2.22.0"
+    }
+  }
+
+  backend "s3" {
+    endpoint                    = "s3.isk01.sakurastorage.jp"
+    region                      = "jp-north-1"
+    bucket                      = "ictsc-k8s-cluster"
+    key                         = "terraform.tfstate"
+    skip_credentials_validation = true
+    skip_region_validation      = true
+  }
+}
+
+# Configure the SakuraCloud Provider
+provider "sakuracloud" {
+}
+
+# ubuntu archive
+data "sakuracloud_archive" "ubuntu-archive" {
+  os_type = "ubuntu2204"
+}
+
+# pub key
+resource "sakuracloud_ssh_key_gen" "gen_key" {
+  name = "k8s_pubkey"
+
+  provisioner "local-exec" {
+    command = "echo \"${self.private_key}\" > id_rsa; chmod 0600 id_rsa"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "rm -f id_rsa"
+  }
+}
+
+resource "sakuracloud_internet" "k8s-external-switch" {
+  name       = "k8s-external-switch"
+  netmask    = lookup(var.external_subnet, terraform.workspace)
+  band_width = 100
+  timeouts {
+    create = "1h"
+    delete = "1h"
+  }
+}
+
+resource "sakuracloud_subnet" "bgp-subnet" {
+  internet_id = sakuracloud_internet.k8s-external-switch.id
+  next_hop    = sakuracloud_internet.k8s-external-switch.ip_addresses[length(sakuracloud_internet.k8s-external-switch.ip_addresses) - 11]
+  timeouts {
+    create = "1h"
+    delete = "1h"
+  }
+}
+
+resource "sakuracloud_switch" "k8s-internal-switch" {
+  name = "k8s-internal-switch"
+  timeouts {
+    create = "1h"
+    delete = "1h"
+  }
+}
+
+# disks
+resource "sakuracloud_disk" "k8s-router-disk" {
+  count             = lookup(var.router, terraform.workspace)
+  name              = "k8s-router-${count.index + 1}-${terraform.workspace}"
+  source_archive_id = lookup(var.router_archive_id, terraform.workspace)
+  size              = lookup(var.router_disk, terraform.workspace)
+  tags              = ["k8s", "${terraform.workspace}"]
+}
+
+resource "sakuracloud_disk" "k8s-master-disk" {
+  count             = lookup(var.master, terraform.workspace)
+  name              = "k8s-master-${count.index + 1}-${terraform.workspace}"
+  source_archive_id = data.sakuracloud_archive.ubuntu-archive.id
+  size              = lookup(var.master_disk, terraform.workspace)
+  tags              = ["k8s", "${terraform.workspace}"]
+  timeouts {
+    create = "1h"
+    delete = "1h"
+  }
+}
+
+resource "sakuracloud_disk" "k8s-node-disk" {
+  count             = lookup(var.node, terraform.workspace)
+  name              = "k8s-node-${count.index + 1}-${terraform.workspace}"
+  source_archive_id = data.sakuracloud_archive.ubuntu-archive.id
+  size              = lookup(var.node_disk, terraform.workspace)
+  tags              = ["k8s", "${terraform.workspace}"]
+  timeouts {
+    create = "1h"
+    delete = "1h"
+  }
+}
+
+resource "sakuracloud_disk" "k8s-rook-disk" {
+  count = lookup(var.node, terraform.workspace)
+  name  = "k8s-rook-${count.index + 1}-${terraform.workspace}"
+  size  = lookup(var.node_rook_disk, terraform.workspace)
+  tags  = ["k8s", "${terraform.workspace}"]
+  timeouts {
+    create = "1h"
+    delete = "1h"
+  }
+}
+
+resource "sakuracloud_disk" "k8s-lb-disk" {
+  count             = lookup(var.lb, terraform.workspace)
+  name              = "k8s-lb-${count.index + 1}-${terraform.workspace}"
+  source_archive_id = data.sakuracloud_archive.ubuntu-archive.id
+  size              = lookup(var.lb_disk, terraform.workspace)
+  tags              = ["k8s", "${terraform.workspace}"]
+  timeouts {
+    create = "1h"
+    delete = "1h"
+  }
+}
+
+# servers
+resource "sakuracloud_server" "k8s-router" {
+  count  = lookup(var.router, terraform.workspace)
+  name   = "k8s-router-${count.index + 1}-${terraform.workspace}"
+  core   = lookup(var.router_cpu, terraform.workspace)
+  memory = lookup(var.router_mem, terraform.workspace)
+  disks  = ["${sakuracloud_disk.k8s-router-disk[count.index].id}"]
+  tags   = ["k8s", "${terraform.workspace}", "@nic-double-queue"]
+
+  network_interface {
+    upstream = sakuracloud_internet.k8s-external-switch.switch_id
+  }
+
+  disk_edit_parameter {
+    hostname        = "k8s-router-${count.index + 1}-${terraform.workspace}"
+    password        = var.cluster_pass
+    disable_pw_auth = "false"
+    ip_address      = sakuracloud_internet.k8s-external-switch.ip_addresses[count.index]
+    gateway         = sakuracloud_internet.k8s-external-switch.gateway
+    netmask         = lookup(var.external_subnet, terraform.workspace)
+  }
+  timeouts {
+    create = "1h"
+    delete = "1h"
+  }
+}
+
+resource "sakuracloud_server" "k8s-master-server" {
+  count  = lookup(var.master, terraform.workspace)
+  name   = "k8s-master-${count.index + 1}-server-${terraform.workspace}"
+  core   = lookup(var.master_cpu, terraform.workspace)
+  memory = lookup(var.master_mem, terraform.workspace)
+  disks  = ["${sakuracloud_disk.k8s-master-disk[count.index].id}"]
+  tags   = ["k8s", "${terraform.workspace}", "@nic-double-queue"]
+
+  network_interface {
+    upstream = sakuracloud_internet.k8s-external-switch.switch_id
+  }
+  network_interface {
+    upstream        = sakuracloud_switch.k8s-internal-switch.id
+    user_ip_address = "192.168.100.1${count.index}"
+  }
+  disk_edit_parameter {
+    hostname        = "k8s-master-${count.index + 1}-server-${terraform.workspace}"
+    password        = var.cluster_pass
+    disable_pw_auth = "true"
+    ssh_key_ids     = ["${sakuracloud_ssh_key_gen.gen_key.id}"]
+    ip_address      = sakuracloud_internet.k8s-external-switch.ip_addresses[count.index + lookup(var.router, terraform.workspace)]
+    gateway         = sakuracloud_internet.k8s-external-switch.gateway
+    netmask         = lookup(var.external_subnet, terraform.workspace)
+  }
+  timeouts {
+    create = "1h"
+    delete = "1h"
+  }
+}
+
+resource "sakuracloud_server" "k8s-node-server" {
+  count  = lookup(var.node, terraform.workspace)
+  name   = "k8s-node-${count.index + 1}-server-${terraform.workspace}"
+  core   = lookup(var.node_cpu, terraform.workspace)
+  memory = lookup(var.node_mem, terraform.workspace)
+  disks  = ["${sakuracloud_disk.k8s-node-disk[count.index].id}", "${sakuracloud_disk.k8s-rook-disk[count.index].id}"]
+  tags   = ["k8s", "${terraform.workspace}", "@nic-double-queue"]
+  network_interface {
+    upstream = sakuracloud_internet.k8s-external-switch.switch_id
+  }
+  network_interface {
+    upstream        = sakuracloud_switch.k8s-internal-switch.id
+    user_ip_address = "192.168.100.2${count.index}"
+  }
+  disk_edit_parameter {
+    hostname        = "k8s-node-${count.index + 1}-server-${terraform.workspace}"
+    password        = var.cluster_pass
+    disable_pw_auth = "true"
+    ssh_key_ids     = ["${sakuracloud_ssh_key_gen.gen_key.id}"]
+    # note_ids        = ["<ID>", "<ID>"]
+    ip_address = sakuracloud_internet.k8s-external-switch.ip_addresses[count.index + length(sakuracloud_internet.k8s-external-switch.ip_addresses) - 4]
+    gateway    = sakuracloud_internet.k8s-external-switch.gateway
+    netmask    = lookup(var.external_subnet, terraform.workspace)
+  }
+  timeouts {
+    create = "1h"
+    delete = "1h"
+  }
+}
+
+resource "sakuracloud_server" "k8s-lb-server" {
+  count  = lookup(var.lb, terraform.workspace)
+  name   = "k8s-lb-${count.index + 1}-server-${terraform.workspace}"
+  core   = lookup(var.lb_cpu, terraform.workspace)
+  memory = lookup(var.lb_mem, terraform.workspace)
+  disks  = ["${sakuracloud_disk.k8s-lb-disk[count.index].id}"]
+  tags   = ["k8s", "${terraform.workspace}", "@nic-double-queue"]
+  network_interface {
+    upstream = sakuracloud_internet.k8s-external-switch.switch_id
+  }
+  network_interface {
+    upstream        = sakuracloud_switch.k8s-internal-switch.id
+    user_ip_address = "192.168.100.3${count.index}"
+  }
+  disk_edit_parameter {
+    hostname        = "k8s-lb-${count.index + 1}-server-${terraform.workspace}"
+    password        = var.cluster_pass
+    disable_pw_auth = "true"
+    ssh_key_ids     = ["${sakuracloud_ssh_key_gen.gen_key.id}"]
+    # note_ids      = ["<ID>", "<ID>"]
+    ip_address = sakuracloud_internet.k8s-external-switch.ip_addresses[count.index + lookup(var.router, terraform.workspace) + lookup(var.master, terraform.workspace)]
+    gateway    = sakuracloud_internet.k8s-external-switch.gateway
+    netmask    = lookup(var.external_subnet, terraform.workspace)
+  }
+  timeouts {
+    create = "1h"
+    delete = "1h"
+  }
+}
+
+# output
+output "show_workspaces" {
+  value = terraform.workspace
+}
+
+output "vip_address" {
+  value = sakuracloud_internet.k8s-external-switch.ip_addresses[lookup(var.router, terraform.workspace) + lookup(var.master, terraform.workspace) + lookup(var.lb, terraform.workspace)]
+}
+
+output "metallb_address_range" {
+  value = "${sakuracloud_subnet.bgp-subnet.ip_addresses[0]}/${lookup(var.external_subnet, terraform.workspace)}"
+}
+
+output "k8s_router_ipaddress" {
+  value = sakuracloud_server.k8s-router.*.ip_address
+}
+
+output "k8s_bgp_router_ipaddress" {
+  value = sakuracloud_server.k8s-router.*.ip_address
+}
+
+output "k8s_master_server_ipaddress" {
+  value = sakuracloud_server.k8s-master-server.*.ip_address
+}
+
+output "k8s_node_server_ipaddress" {
+  value = sakuracloud_server.k8s-node-server.*.ip_address
+}
+
+output "k8s_lb_server_ipaddress" {
+  value = sakuracloud_server.k8s-lb-server.*.ip_address
+}
+
+output "public_address_list" {
+  value = sakuracloud_internet.k8s-external-switch.ip_addresses
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,180 @@
+# disk size plan: https://cloud.sakura.ad.jp/specification/server-disk/#server-disk-content02-price
+variable "cluster_pass" { default = "" }
+variable "external_subnet" {
+  type = map(any)
+  default = {
+    wsp = 28
+    dev = 28
+    prd = 28
+  }
+}
+variable "master" {
+  type = map(any)
+  default = {
+    wsp = 3
+    dev = 3
+    prd = 3
+  }
+}
+variable "master_cpu" {
+  type = map(any)
+  default = {
+    wsp = 4
+    dev = 4
+    prd = 4
+  }
+}
+variable "master_mem" {
+  type = map(any)
+  default = {
+    wsp = 8
+    dev = 8
+    prd = 8
+  }
+}
+variable "master_disk" {
+  type = map(any)
+  default = {
+    wsp = 40
+    dev = 40
+    prd = 40
+  }
+}
+
+variable "node" {
+  type = map(any)
+  default = {
+    wsp = 3
+    dev = 3
+    prd = 3
+  }
+}
+variable "node_cpu" {
+  type = map(any)
+  default = {
+    wsp = 4
+    dev = 4
+    prd = 4
+  }
+}
+variable "node_mem" {
+  type = map(any)
+  default = {
+    wsp = 16
+    dev = 8
+    prd = 16
+  }
+}
+variable "node_disk" {
+  type = map(any)
+  default = {
+    wsp = 40
+    dev = 40
+    prd = 40
+  }
+}
+
+variable "node_rook_disk" {
+  type = map(any)
+  default = {
+    wsp = 100
+    dev = 40
+    prd = 100
+  }
+}
+
+variable "lb" {
+  type = map(any)
+  default = {
+    wsp = 2
+    dev = 2
+    prd = 2
+  }
+}
+variable "lb_cpu" {
+  type = map(any)
+  default = {
+    wsp = 2
+    dev = 2
+    prd = 2
+  }
+}
+variable "lb_mem" {
+  type = map(any)
+  default = {
+    wsp = 2
+    dev = 2
+    prd = 2
+  }
+}
+variable "lb_disk" {
+  type = map(any)
+  default = {
+    wsp = 20
+    dev = 20
+    prd = 20
+  }
+}
+
+
+
+variable "first_step_subnet" {
+  type = map(any)
+  default = {
+    wsp = "28"
+    dev = "28"
+    prd = "28"
+  }
+}
+
+variable "router_archive_id" {
+  type = map(any)
+  default = {
+    wsp = "113500057368"
+    dev = "113500057368"
+    prd = "113500057368"
+  }
+}
+
+variable "router" {
+  type = map(any)
+  default = {
+    wsp = "1"
+    dev = "1"
+    prd = "1"
+    bgp = "1"
+  }
+}
+
+variable "router_cpu" {
+  type = map(any)
+  default = {
+    wsp = "2"
+    dev = "2"
+    prd = "2"
+  }
+}
+variable "router_mem" {
+  type = map(any)
+  default = {
+    wsp = "2"
+    dev = "2"
+    prd = "2"
+  }
+}
+variable "router_disk" {
+  type = map(any)
+  default = {
+    wsp = "20"
+    dev = "20"
+    prd = "20"
+  }
+}
+variable "router_subnet" {
+  type = map(any)
+  default = {
+    wsp = 28
+    dev = 28
+    prd = 28
+  }
+}
