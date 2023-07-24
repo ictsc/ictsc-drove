da87eb9f451b6118150f8369f7a5327ca02c783b
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-07-25 06:08:50 +0900

    eliminated redundant output

--- terraform/main.tf
+++ terraform/main.tf
@@ -241,38 +241,27 @@ resource "sakuracloud_server" "k8s-lb-server" {
 }
 
 # output
-output "show_workspaces" {
-  value = terraform.workspace
-}
 
 output "vip_address" {
   value = sakuracloud_internet.k8s-external-switch.ip_addresses[lookup(var.router, terraform.workspace) + lookup(var.master, terraform.workspace) + lookup(var.lb, terraform.workspace)]
 }
 
-output "metallb_address_range" {
+output "external_address_range" {
   value = "${sakuracloud_subnet.bgp-subnet.ip_addresses[0]}/${lookup(var.external_subnet, terraform.workspace)}"
 }
 
-output "k8s_router_ipaddress" {
+output "k8s_router_ip_address" {
   value = sakuracloud_server.k8s-router.*.ip_address
 }
 
-output "k8s_bgp_router_ipaddress" {
-  value = sakuracloud_server.k8s-router.*.ip_address
-}
-
-output "k8s_master_server_ipaddress" {
+output "k8s_master_server_ip_address" {
   value = sakuracloud_server.k8s-master-server.*.ip_address
 }
 
-output "k8s_node_server_ipaddress" {
+output "k8s_node_server_ip_address" {
   value = sakuracloud_server.k8s-node-server.*.ip_address
 }
 
-output "k8s_lb_server_ipaddress" {
+output "k8s_lb_server_ip_address" {
   value = sakuracloud_server.k8s-lb-server.*.ip_address
-}
-
-output "public_address_list" {
-  value = sakuracloud_internet.k8s-external-switch.ip_addresses
 }
