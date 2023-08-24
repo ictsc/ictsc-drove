f06939df7f70bcdebabf514425a86b8d9f8477d8
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-08-24 12:58:56 +0900

    using square brackets

--- terraform/output.tf
+++ terraform/output.tf
@@ -7,17 +7,17 @@ output "external_address_range" {
 }
 
 output "k8s_router_ip_address" {
-  value = sakuracloud_server.k8s-router.*.ip_address
+  value = sakuracloud_server.k8s-router[*].ip_address
 }
 
 output "k8s_master_server_ip_address" {
-  value = sakuracloud_server.k8s-master-server.*.ip_address
+  value = sakuracloud_server.k8s-master-server[*].ip_address
 }
 
 output "k8s_node_server_ip_address" {
-  value = sakuracloud_server.k8s-node-server.*.ip_address
+  value = sakuracloud_server.k8s-node-server[*].ip_address
 }
 
 output "k8s_lb_server_ip_address" {
-  value = sakuracloud_server.k8s-lb-server.*.ip_address
+  value = sakuracloud_server.k8s-lb-server[*].ip_address
 }
