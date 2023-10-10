482d715182019fe4d19d3d200df45a4279b8b977
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-10-10 15:07:19 +0900

    use full url style

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 
   backend "s3" {
     endpoints {
-      s3 = "https://s3.isk01.sakurastorage.jp/"
+      s3 = "https://s3.isk01.sakurastorage.jp"
     }
     region                      = "jp-north-1"
     key                         = "terraform.tfstate"
