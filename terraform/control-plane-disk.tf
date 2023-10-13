e7f725adf46d72e8d21ab6c541d8880aa5f51273
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-10-13 15:34:57 +0900

    rename archive

--- terraform/control-plane-disk.tf
+++ terraform/control-plane-disk.tf
@@ -1,7 +1,7 @@
 resource "sakuracloud_disk" "k8s_control_plane_disk" {
   count             = lookup(var.control_plane, terraform.workspace)
   name              = "k8s_control_plane_${count.index + 1}_${terraform.workspace}"
-  source_archive_id = data.sakuracloud_archive.ubuntu-archive.id
+  source_archive_id = data.sakuracloud_archive.ubuntu_archive.id
   size              = lookup(var.control_plane_disk, terraform.workspace)
   tags              = ["k8s", terraform.workspace]
   timeouts {
--- terraform/router-disk.tf
+++ terraform/router-disk.tf
@@ -1,7 +1,7 @@
 resource "sakuracloud_disk" "k8s_router_disk" {
   count             = lookup(var.router, terraform.workspace)
   name              = "k8s_router_${count.index + 1}_${terraform.workspace}"
-  source_archive_id = data.sakuracloud_archive.ubuntu-archive.id
+  source_archive_id = data.sakuracloud_archive.ubuntu_archive.id
   size              = lookup(var.router_disk, terraform.workspace)
   tags              = ["k8s", terraform.workspace]
   timeouts {
