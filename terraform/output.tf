61ffc65d3e28a2bf17b3e593fbb59a60fef17ac9
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-12-25 05:48:58 +0900

    changed ip address allocation

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,9 @@
 output "vip_address" {
-  value = sakuracloud_internet.k8s_external_switch.ip_addresses[lookup(var.router, terraform.workspace, 0) + lookup(var.control_plane, terraform.workspace, 0)]
+  value = sakuracloud_internet.k8s_external_switch.ip_addresses[lookup(var.router, terraform.workspace, 0) + lookup(var.control_plane, terraform.workspace, 0) + lookup(var.worker_node, terraform.workspace, 0)]
 }
 
 output "external_address_range" {
-  value = format("%s/%s", sakuracloud_subnet.bgp_subnet.ip_addresses[0], lookup(var.external_subnet, terraform.workspace, 0))
+  value = format("%s/%s", sakuracloud_subnet.bgp_subnet.ip_addresses[0], 28)
 }
 
 output "k8s_router_ip_address" {
--- terraform/worker-node-server.tf
+++ terraform/worker-node-server.tf
@@ -18,7 +18,7 @@ resource "sakuracloud_server" "k8s_worker_node" {
     disable_pw_auth = "true"
     ssh_key_ids     = [sakuracloud_ssh_key_gen.gen_key.id]
     # note_ids        = ["<ID>", "<ID>"]
-    ip_address = sakuracloud_internet.k8s_external_switch.ip_addresses[count.index + length(sakuracloud_internet.k8s_external_switch.ip_addresses) - 4]
+    ip_address = sakuracloud_internet.k8s_external_switch.ip_addresses[count.index + lookup(var.router, terraform.workspace, 0) + lookup(var.control_plane, terraform.workspace, 0)]
     gateway    = sakuracloud_internet.k8s_external_switch.gateway
     netmask    = lookup(var.external_subnet, terraform.workspace, 0)
   }
