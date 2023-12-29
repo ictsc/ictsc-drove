82ff20232fff39bc163d2a074a87714a9a63c7b1
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-12-29 14:33:43 +0900

    fix git clone & clean up

--- manifest/common/autoscaler/pre-validation.sh
+++ manifest/common/autoscaler/pre-validation.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-git clone https://github.com/kubernetes/autoscaler.git
+git clone https://github.com/kubernetes/autoscaler.git --depth 1
 ./autoscaler/vertical-pod-autoscaler/hack/vpa-up.sh
+rm -rf autoscaler
