02a482e093a94a17e8fd97c9dfd60ad475e0cf13
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-02-02 04:21:56 +0900

    add workspace to switch name

--- terraform/network-external.tf
+++ terraform/network-external.tf
@@ -1,5 +1,5 @@
 resource "sakuracloud_internet" "k8s_external_switch" {
-  name       = "k8s-external-switch"
+  name       = "k8s-${terraform.workspace}-external-switch"
   netmask    = lookup(var.external_subnet, terraform.workspace, 0)
   band_width = 100
   timeouts {
--- terraform/network-internal.tf
+++ terraform/network-internal.tf
@@ -1,5 +1,5 @@
 resource "sakuracloud_switch" "k8s_internal_switch" {
-  name = "k8s-internal-switch"
+  name = "k8s-${terraform.workspace}-internal-switch"
   timeouts {
     create = "1h"
     delete = "1h"
