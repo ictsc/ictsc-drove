f7f4f29bdccd9d9bccb6c350fb4070a219a03579
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-10-10 22:10:51 +0900

    fix keys.sh

--- dev/keys.sh
+++ dev/keys.sh
@@ -2,4 +2,4 @@
 
 curl -sS https://raw.githubusercontent.com/ictsc/ictsc-github-member/production/terraform.tfstate |
   jq -r '.resources[] | select( .type == "github_team_members").instances[] | select( .index_key | contains ("ictsc2024") ).attributes.members[] | .username' |
-  while read -r line; do curl -q "https://github.com/${line}.keys" | grep "ssh-" || true; done >./keys
+  while read -r line; do curl -q "https://github.com/${line}.keys" | grep -e "ssh-" -e "ecdsa-" || true; done >./keys
