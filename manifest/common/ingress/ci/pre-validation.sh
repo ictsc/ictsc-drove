1c8a150f9b05636195955abe4ff8f7bbe610a610
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-10-21 02:32:29 +0900

    disable test

--- manifest/common/ingress/ci/pre-validation.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-kubectl apply -f https://raw.githubusercontent.com/cert-manager/cert-manager/refs/heads/master/deploy/crds/crd-certificates.yaml
-kubectl apply -f https://raw.githubusercontent.com/traefik/traefik-helm-chart/refs/heads/master/traefik/crds/traefik.io_ingressroutes.yaml
