80e91cb83bbca6cadcea48ca6cd000c1d18283cd
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-10-22 03:14:22 +0900

    add assignable min & max address

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,6 +2,14 @@ output "vip_address" {
   value = sakuracloud_internet.k8s_external_switch.ip_addresses[lookup(var.control_plane, terraform.workspace, 0)]
 }
 
+output "min_ip_address" {
+  value = sakuracloud_internet.k8s_external_switch.ip_addresses[lookup(var.control_plane, terraform.workspace, 0) + 1]
+}
+
+output "max_ip_address" {
+  value = sakuracloud_internet.k8s_external_switch.max_ip_address
+}
+
 output "k8s_control_plane_ip_address" {
   value = sakuracloud_server.k8s_control_plane[*].ip_address
 }
