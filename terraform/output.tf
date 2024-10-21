4787d44b6febbdf293737ef6b2d347cfcb96f8ad
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-10-22 03:11:41 +0900

    delete subnet

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,10 +2,6 @@ output "vip_address" {
   value = sakuracloud_internet.k8s_external_switch.ip_addresses[lookup(var.control_plane, terraform.workspace, 0)]
 }
 
-output "external_address_range" {
-  value = format("%s/%s", sakuracloud_subnet.bgp_subnet.ip_addresses[0], 28)
-}
-
 output "k8s_control_plane_ip_address" {
   value = sakuracloud_server.k8s_control_plane[*].ip_address
 }
