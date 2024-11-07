c0b1cf07e1d20558ae035591f11fdd6acf74dc65
tosuke <13393900+tosuke@users.noreply.github.com>
2024-11-07 13:32:28 +0900

    feat: copy records from ictsc/terraform-ictsc-net

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -9,3 +9,27 @@ resource "sakuracloud_dns_record" "k8s_apiserver_record" {
   type   = "A"
   value  = sakuracloud_internet.k8s_external_switch.ip_addresses[lookup(var.control_plane, terraform.workspace, 0)]
 }
+
+resource "sakuracloud_dns_record" "root_record_ipv4" {
+  dns_id = sakuracloud_dns.k8s_dns.id
+  ttl    = 300
+  name   = "@"
+  type   = "A"
+  value  = "163.43.138.216"
+}
+
+resource "sakuracloud_dns_record" "root_record_ipv6" {
+  dns_id = sakuracloud_dns.k8s_dns.id
+  ttl    = 300
+  name   = "@"
+  type   = "AAAA"
+  value  = "2001:e42:407:1035::2:0"
+}
+
+resource "sakuracloud_dns_record" "wildcard_record" {
+  dns_id = sakuracloud_dns.k8s_dns.id
+  ttl    = 300
+  name   = "*"
+  type   = "CNAME"
+  value  = sakuracloud_dns.k8s_dns.zone
+}
