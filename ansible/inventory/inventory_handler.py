13e9a376459c7e88d65a95bf32cd1045cc1c0803
logica0419 <logica0419@gmail.com>
2025-04-08 23:10:58 +0900

    Revert "fetch ssh private key from tfstate"
    This reverts commit a4590ddacf5439a3d4a4467a926ece856ae664b3.

--- ansible/inventory/inventory_handler.py
+++ ansible/inventory/inventory_handler.py
@@ -36,12 +36,6 @@ def get_workspace():
     )
 
 
-def create_ssh_key(value: str, workspace: str):
-    with open(f"{os.path.dirname(__file__)}/../../id_rsa_{workspace}", "w") as f:
-        f.write(value)
-    os.chmod(f"{os.path.dirname(__file__)}/../../id_rsa_{workspace}", 0o600)
-
-
 def main():
     inventory = {
         "all": {},
@@ -103,7 +97,6 @@ def main():
                     worker_node += 1
 
     inventory["all"]["vars"] = {
-        "ansible_ssh_private_key_file": f"{os.path.dirname(__file__)}/../../id_rsa_{workspace}",
         "ipv6_prefix": ipv6_prefix,
         "ipv6_prefix_len": tfstate["outputs"]["ipv6_prefix_len"]["value"],
     }
@@ -124,14 +117,32 @@ def main():
             ),
         },
     }
-    inventory["worker_node"]["vars"] = {
-        "ansible_ssh_common_args": (
-            "-o ProxyCommand='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "
-            f"-i ../id_rsa_{workspace} -W %h:%p ubuntu@{inventory['control_plane']['hosts'][0]}'"
-        )
-    }
 
-    create_ssh_key(tfstate["outputs"]["ssh_private_key"]["value"], workspace)
+    # SSH鍵が存在するかによって、認証情報を分ける
+    #  Terraformによって作られたSSH鍵が存在しない場合、ictscユーザーに切り替えて
+    #  GitHubに登録されているそれぞれのメンバーの公開鍵で認証するようにする
+    if os.path.isfile(f"../id_rsa_{workspace}"):
+        inventory["all"]["vars"] |= {
+            "ansible_user": "ubuntu",
+            "ansible_sudo_pass": os.environ.get("CLUSTER_PASS"),
+            "ansible_ssh_private_key_file": f"../id_rsa_{workspace}",
+        }
+        inventory["worker_node"]["vars"] = {
+            "ansible_ssh_common_args": (
+                "-o ProxyCommand='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "
+                f"-i ../id_rsa_{workspace} -W %h:%p ubuntu@{inventory['control_plane']['hosts'][0]}'"
+            )
+        }
+    else:
+        inventory["all"]["vars"] |= {
+            "ansible_user": "ictsc",
+        }
+        inventory["worker_node"]["vars"] = {
+            "ansible_ssh_common_args": (
+                "-o ProxyCommand='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "
+                f"-W %h:%p ictsc@{inventory['control_plane']['hosts'][0]}'"
+            )
+        }
 
     print(json.dumps(inventory))
 
