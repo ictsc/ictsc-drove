825f1e3a55900f17ed18b28a00a6066edaa50854
logica0419 <logica0419@gmail.com>
2025-04-12 02:02:43 +0900

    use ingress route for k8s.drove{-dev}.ictsc.net

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -2,14 +2,6 @@ resource "sakuracloud_dns" "k8s_dns" {
   zone = lookup(var.dns_zone, terraform.workspace, 0)
 }
 
-resource "sakuracloud_dns_record" "k8s_apiserver_record" {
-  dns_id = sakuracloud_dns.k8s_dns.id
-  ttl    = 300
-  name   = "k8s"
-  type   = "A"
-  value  = sakuracloud_internet.k8s_external_switch.ip_addresses[lookup(var.control_plane, terraform.workspace, 0)]
-}
-
 resource "sakuracloud_dns_record" "root_record_ipv4" {
   dns_id = sakuracloud_dns.k8s_dns.id
   ttl    = 300
