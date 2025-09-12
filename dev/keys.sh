6ca5e733b44de7eec742e861122edd955baafedd
Takuto Nagami <logica0419@gmail.com>
2025-09-12 16:04:07 +0900

    update versions

--- dev/keys.sh
+++ dev/keys.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl -sS https://raw.githubusercontent.com/ictsc/ictsc-github-member/production/terraform.tfstate |
-  jq -r '.resources[] | select( .type == "github_team_members").instances[] | select( .index_key | contains ("ictsc2024") ).attributes.members[] | .username' |
+  jq -r '.resources[] | select( .type == "github_team_members").instances[] | select( .index_key | contains ("ictsc2025") ).attributes.members[] | .username' |
   while read -r line; do curl -q "https://github.com/${line}.keys" | grep -e "ssh-" -e "ecdsa-" || true; done >./keys
